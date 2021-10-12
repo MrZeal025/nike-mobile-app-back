0399e4439da70e7d4d30095f55daf7655c86571e
MrZeal025 <ryannograles.025.gtis@gmail.com>
2021-10-12 13:06:00 +0800

    added mongo db connection and server setup

--- src/configs/dbConf.ts
+++ src/configs/dbConf.ts
@@ -1,30 +1,19 @@
 import mongoose from "mongoose";
 
-const mongooseConnector = async (
-	mongodbUri: string,
-	user: string,
-	pass: string
-) => {
+const mongooseConnector = async (mongodbUri: string) => {
 	let connected = false;
-
-	const databaseOptions = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-		useCreateIndex: true,
-		authSource: "admin",
-		user,
-		pass,
-	};
 
 	while (!connected) {
 		await mongoose
-			.connect(mongodbUri, databaseOptions)
+			.connect(mongodbUri, {
+				useUnifiedTopology: true,
+				useNewUrlParser: true,
+			})
 			.then(() => {
 				connected = true;
 				console.log("Successfully connected to the database.\n\n");
 			})
-			.catch(() => {
+			.catch((err) => {
 				console.error(
 					"We have a problem connecting to the database, retrying..."
 				);
@@ -48,11 +37,11 @@ export default async function connectToDatabase(): Promise<boolean> {
 		);
 	}
 
-	const mongodbUri = `mongodb://${DB_HOST}/${DB_NAME}`;
+	const mongodbUri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 	console.log(
 		`Connecting to the MongoDB database at: mongodb://${DB_HOST}:${DB_PORT}`
 	);
 
-	return await mongooseConnector(mongodbUri, DB_USER, DB_PASS);
+	return await mongooseConnector(mongodbUri);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,45 @@
+import { clientHandler } from "configs/clientHandler";
 import dotenv from "dotenv";
-import dbConnect from "./configs/dbConf";
+import dbConnect from "configs/dbConf";
 import express from "express";
+import morgan from "morgan";
 
+// main router
+import { mainRouter } from "./routes";
+
+// Reads and parse env files
 dotenv.config();
+
+// start up
+const app = express();
+
+// constancts and defaults
+const port = process.env.PORT || 5000;
+
+// app usages
+app.use(morgan("combined"));
+app.use(express.json({ limit: "50mb" }));
+app.use("/api", mainRouter);
+// app.use(clientHandler);
+
+// starting the server
+const startServer = async () => {
+	console.clear();
+	console.log("Practise server for the mobile app nike shoes. \n");
+
+	if (await dbConnect()) {
+		app.listen(port, () => {
+			console.log(
+				`Now running and listening at \x1b[32m${
+					process.env.NODE_ENV === "development"
+						? "localhost:"
+						: "SERVER-IP"
+				}${port}`,
+				"\x1b[0m"
+			);
+		});
+		console.log("Sever logging starts now.");
+	}
+};
+
+startServer();
